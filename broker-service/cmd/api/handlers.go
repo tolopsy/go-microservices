c05f6fe449f967ffb3f3192c5b2ec1aecafa3129
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2023-11-07 12:13:48 +0100

    feat: route log requests to log service

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -10,11 +10,17 @@ import (
 type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
+	Log    LogPayload  `json:"log,omitempty"`
 }
 
 type AuthPayload struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LogPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -37,13 +43,47 @@ func (app *Config) HandleRequest(w http.ResponseWriter, r *http.Request) {
 	switch requestPayload.Action {
 	case "auth":
 		app.authenticate(w, requestPayload.Auth)
+	case "log":
+		app.logItem(w, requestPayload.Log)
 	default:
 		app.errorJson(w, errors.New("unknown action"))
 	}
 }
 
+func (app *Config) logItem(w http.ResponseWriter, entry LogPayload) {
+	jsonData, _ := json.Marshal(entry)
+	request, err := http.NewRequest("POST", "http://log-service/log", bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJson(w, err)
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+
+	if err != nil {
+		app.errorJson(w, err)
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJson(w, errors.New("error calling auth service"))
+		return
+	}
+
+	payload := jsonResponse{
+		Error:   false,
+		Message: "logged",
+	}
+
+	app.writeJson(w, http.StatusAccepted, payload)
+}
+
 func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
-	jsonData, _ := json.MarshalIndent(a, "", "\t")
+	jsonData, _ := json.Marshal(a)
 	request, err := http.NewRequest("POST", "http://authentication-service/authenticate", bytes.NewBuffer(jsonData))
 	if err != nil {
 		app.errorJson(w, err)
@@ -80,10 +120,10 @@ func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 	}
 
 	payload := jsonResponse{
-		Error: false,
+		Error:   false,
 		Message: "Authenticated",
-		Data: jsonFromAuthService.Data,
+		Data:    jsonFromAuthService.Data,
 	}
-	
+
 	app.writeJson(w, http.StatusAccepted, payload)
 }
