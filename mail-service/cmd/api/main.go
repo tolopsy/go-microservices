2490d7569e1a3e66c243b3302cf07305a48c071b
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2023-11-13 06:22:13 +0100

    feat: add mail templates, logic and req handler

--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -4,15 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 type Config struct {
+	Mailer Mail
 }
 
 const webPort = "80"
 
 func main() {
-	app := Config{}
+	app := Config{
+		Mailer: createMail(),
+	}
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
 		Handler: app.routes(),
@@ -22,4 +27,19 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
+}
+
+func createMail() Mail {
+	port, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	m := Mail{
+		Domain: os.Getenv("MAIL_DOMAIN"),
+		Host: os.Getenv("MAIL_HOST"),
+		Port: port,
+		Username: os.Getenv("MAIL_USERNAME"),
+		Password: os.Getenv("MAIL_PASSWORD"),
+		FromAddress: os.Getenv("MAIL_FROM_ADDRESS"),
+		FromName: os.Getenv("MAIL_FROM_NAME"),
+		Encryption: os.Getenv("MAIL_ENCRYPTION"),
+	}
+	return m
 }
