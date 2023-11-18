892c53db11fff7c1f20d2f9c2760cfb55350fc58
Tolulope Olanrewaju <lanre.toluwa@gmail.com>
2023-11-18 17:10:23 +0100

    feat: setup rabbitmq connection for broker service

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -3,26 +3,64 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"net/http"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 const webPort = "80"
 
-type Config struct {}
+type Config struct {
+	amqpConn *amqp.Connection
+}
 
 func main() {
-	// TODO: To be used or removed
-	app := Config{}
+	// connect to rabbitmq
+	amqpConn, err := connect()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer amqpConn.Close()
+	app := Config{
+		amqpConn: amqpConn,
+	}
 
 	log.Printf("Starting broker service on port %s\n", webPort)
 
 	server := &http.Server{
-		Addr: fmt.Sprintf(":%s", webPort),
+		Addr:    fmt.Sprintf(":%s", webPort),
 		Handler: app.routes(),
 	}
 
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
+	}
+}
+
+// Connecting to the RabbitMQ with exponential backoff retry until successful or maximum retries are reached.
+func connect() (*amqp.Connection, error) {
+	AMQP_URL := os.Getenv("AMQP_URL")
+	var retryCount int64
+	const maxRetries = 5
+
+	for {
+		connection, err := amqp.Dial(AMQP_URL)
+		if err == nil {
+			return connection, nil
+		}
+
+		retryCount++
+		if retryCount > maxRetries {
+			return nil, err
+		}
+
+		backoff := time.Duration(math.Pow(float64(retryCount), 2)) * time.Second
+		log.Printf("Backing off! Retrying in %s", backoff)
+		time.Sleep(backoff)
+		continue
 	}
 }
---
+++ broker-service/event/event.go
@@ -0,0 +1,58 @@
+package event
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const EXCHANGE_NAME = "logs_topic"
+
+func declareExchange(channel *amqp.Channel) error {
+	return channel.ExchangeDeclare(
+		EXCHANGE_NAME, // name
+		"topic",       // type
+		true,          // durable?
+		false,         // auto delete?
+		false,         // internal?
+		false,         // no-wait?
+		nil,           // args
+	)
+}
+
+func declareQueue(channel *amqp.Channel) (amqp.Queue, error) {
+	return channel.QueueDeclare(
+		"",    // name
+		false, // durable?
+		false, // auto delete?
+		true,  // exclusive,
+		false, // no-wait?
+		nil,   // args
+	)
+}
+
+func logEvent(payload Payload) error {
+	jsonData, _ := json.Marshal(payload)
+	request, err := http.NewRequest("POST", "http://log-service/log", bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusAccepted {
+		return err
+	}
+
+	return nil
+}
